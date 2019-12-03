f30e3212b438d3f6da6fab7649f6b74649b0fefe
Jephtah <jephtino@gmail.com>
2019-12-03 13:51:36 +0100

    fixed cors

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,12 +1,12 @@
+const express = require('express')
+const cors = require('cors')
 const error = require('../middleware/error')
 const users = require('../routes/users')
 const transactions = require('../routes/transactions')
 const trips = require('../routes/trip')
-const express = require('express')
-const cors = require('cors')
 
 module.exports = function (app) {
-  app.use(cors({origin: '*'}))
+  app.use(cors({ origin: '*' }))
   app.use(express.json())
   app.use(error)
   app.use('/api/users', users)
