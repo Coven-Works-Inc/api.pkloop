b2626960c03a02b0a79c5bed9ea048614426268f
Jephtah <jephtino@gmail.com>
2019-12-03 15:29:51 +0100

    fixed cors

--- src/server.js
+++ src/server.js
@@ -10,17 +10,17 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth'
-  )
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE')
-    res.status(200).json({})
-  }
-})
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE')
+//     res.status(200).json({})
+//   }
+// })
 
 require('./startup/logging')()
 require('./startup/routes')(app)
