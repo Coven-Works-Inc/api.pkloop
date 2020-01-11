be7512436c9e124b4eb595cd34e60a74b2d5d330
Jephtah <jephtino@gmail.com>
2020-01-11 09:03:03 +0100

    fixed id

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,11 +2,11 @@ const User = require('../models/User')
 const cloudinary = require('cloudinary')
 require('dotenv').config
 
-cloudinary.config = ({
+cloudinary.config = {
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
-})
+}
 
 exports.fetchAllUsers = async (req, res) => {
   try {
@@ -29,38 +29,55 @@ exports.fetchAllUsers = async (req, res) => {
 }
 exports.updateUser = async (req, res) => {
   const user = await User.findById(req.user._id)
-  if(!user){
-    return res.status(404).json({ status: true, message: 'No users found'})
+  if (!user) {
+    return res.status(404).json({ status: true, message: 'No users found' })
   } else {
-    if(req.body.email && req.body.firstname && req.body.lastname && req.body.phone){
+    if (
+      req.body.email &&
+      req.body.firstname &&
+      req.body.lastname &&
+      req.body.phone
+    ) {
       user.email = req.body.email
       user.firstname = req.body.firstname
       user.lastname = req.body.lastname
       user.phone = req.body.phone
       user.photo = req.body.photo
-  
+
       await user.save()
       return res.status(200).json({
-        status: true, 
+        status: true,
         data: user
       })
     }
   }
-  
 }
-exports.updateProfilePicture = async(req, res) => {
-    const user = await User.findById(req.user.id)
-    const path = Object.values(Object.values(req.files)[0])[0].path
-    if(!user) return res.status(404).json({message: 'Please login to continue'})
-    await cloudinary.uploader.upload(path)
-    .then(photo => {
-      user.photo = photo
-      await user.save()
-      return res.status(200).json({
-        status: true,
-        message: 'image successfully uploaded',
-        data: user
-      })
+exports.updateProfilePicture = async (req, res) => {
+  const user = await User.findById(req.user._id)
+  const path = Object.values(Object.values(req.files)[0])[0].path
+  if (!user)
+    return res.status(404).json({ message: 'Please login to continue' })
+  await cloudinary.uploader.upload(path).then(async photo => {
+    user.photo = photo
+    await user.save()
+    return res.status(200).json({
+      status: true,
+      message: 'image successfully uploaded',
+      data: user
     })
+  })
+}
 
+exports.updateMyBalance = async (req, res) => {
+  const balance = req.body.balance
+  const user = await User.findById(req.user._id)
+
+  user.balance = user.balance + balance
+
+  await user.save()
+}
+
+exports.reduceMyBalance = async (req, res) => {
+  const balance = req.body.balance
+  const user = await User.findById(req.user.id)
 }
