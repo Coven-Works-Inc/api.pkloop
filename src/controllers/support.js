58dec0da998c15d34837b31b0574ede50d6857d0
Jephtah <jephtino@gmail.com>
2020-01-15 11:41:07 +0100

    modified logic for post posting to support

--- src/controllers/support.js
+++ src/controllers/support.js
@@ -1,36 +1,39 @@
 const Support = require('../models/Support')
 const User = require('../models/User')
+const crypto = require('crypto')
 
-const postSupport = async(req, res) => {
-    const user = await User.findById(req.user._id)
+const postSupport = async (req, res) => {
+  const ticketId = crypto.randomBytes(6).toString('hex')
+  const userId = req.user._id
 
-    const support = new Support({
-        user: user.id,
-        subject: req.body.subject,
-        text: req.body.status,
-        date: Date.now()
-    })
-    await support.save()
+  const support = new Support({
+    user: userId,
+    subject: req.body.subject,
+    ticketId: '#' + ticketId,
+    message: req.body.message,
+    status: 'Open'
+  })
+  await support.save()
 
-    res.status(200).json({status: true, data: support})
+  res.status(200).json({ status: true, message: 'Success', data: support })
 }
 
 const fetchSupport = async (req, res, next) => {
-    let support = await Support.find({ user: req.user._id })
-  
-    let message
-  
-    if (support.length === 0) {
-      support = []
-      message = 'No support ticket found'
-    } else {
-        message = `Found ${support.length} support tickets`
-    }
-  
-    return res.status(200).json({ status: true, support, message })
+  let support = await Support.find({ user: req.user._id })
+
+  let message
+
+  if (support.length === 0) {
+    support = []
+    message = 'No support ticket found'
+  } else {
+    message = `Found ${support.length} support tickets`
   }
 
+  return res.status(200).json({ status: true, support, message })
+}
+
 module.exports = {
-    postSupport,
-    fetchSupport
+  postSupport,
+  fetchSupport
 }
--- src/models/Support.js
+++ src/models/Support.js
@@ -1,28 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose')
 
-const supportSchema = new Schema({
+const supportSchema = new Schema(
+  {
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }, 
-    subject: {
-        type: String, 
-        required: [true, 'Enter a subject']
+      type: Schema.Types.ObjectId,
+      ref: 'User'
     },
-    text: {
-        type: String,
-        required: [true, 'Enter text for support']
+    subject: {
+      type: String,
+      required: [true, 'Enter a subject']
+    },
+    ticketId: {
+      type: String
+    },
+    message: {
+      type: String,
+      required: [true, 'Message is required']
     },
     status: {
-        type: String,
-        enum: ['Open', 'Closed'],
-        default: 'Open'
-    },
-    date: {
-        type: Date,
-        default: Date.now()
+      type: String,
+      enum: ['Open', 'Closed'],
+      default: 'Open'
     }
-})
+  },
+  { timestamps: true }
+)
 const Support = model('Support', supportSchema)
 
 module.exports = Support
