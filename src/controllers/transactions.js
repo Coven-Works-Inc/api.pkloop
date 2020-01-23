506327b170436d976b6a2fae6cfef47af4897ff5
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-01-23 00:18:41 -0800

    Update transaction to return trip

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,27 +1,31 @@
 const Transaction = require('../models/Transaction')
 const User = require('../models/User')
+const Trip = require('../models/Trip')
 
 const postTransaction = async (req, res, next) => {
   const user = await User.findById(req.user._id)
+  const trip = await Trip.findById(req.body.tripId)
 
   const transaction = new Transaction({
     user: user.id,
     status: req.body.status,
     with: req.body.with,
-    role: req.body.role
+    role: req.body.role,
+    tripId: req.body.tripId
   })
   
   const travelerTransaction = new Transaction({
     user: req.body.travelerId,
     status: req.body.status,
     with: req.body.senderName,
-    role: 'Traveller'
+    role: 'Traveller',
+    tripId: req.body.tripId
   })
 
   await transaction.save()
   await travelerTransaction.save()
 
-  res.status(200).json({ status: true, sender: transaction, traveller: travelerTransaction })
+  res.status(200).json({ status: true, sender: transaction, traveller: travelerTransaction, trip})
 }
 const fetchTransactions = async (req, res, next) => {
   const transactions = await Transaction.find()
