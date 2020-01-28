6c83eeb295ef5d9910286311c766a8bce5db80fd
Jephtah <jephtino@gmail.com>
2020-01-28 04:18:11 +0100

    changed auth router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,13 +13,7 @@ authRouter.post('/resend', authController.resendToken)
 authRouter.post('/login', authController.login)
 authRouter.post('/reset', authController.resetpassword)
 authRouter.post('/password/:token', authController.newpassword)
-// authRouter.get(
-//   '/google',
-//   passport.authenticate('google', {
-//     scope: ['profile', 'email']
-//   })
-// )
-authRouter.get('/google', authController.googleauth)
+authRouter.get('/google-login', authController.googleauth)
 authRouter.get('/google/callback', authController.googleCallBack)
 authRouter.post('/update', auth, authController.updatePassword)
 
