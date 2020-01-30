72b0e783697a9589c07afd59a11428ce2e09394b
Jephtah <jephtino@gmail.com>
2020-01-30 05:14:31 +0100

    Mail sending feature for both traveler and sender

--- src/utils/email/trips.js
+++ src/utils/email/trips.js
@@ -12,43 +12,39 @@ const sendMail = async (
   message,
   template
 ) => {
-  let emailData
+  let emailData = null
   switch (template) {
     case 'senderAccept':
       emailData = {
         from: process.env.EMAIL_FROM,
         to: senderemail,
-        subject: `Request Update`,
+        subject: 'Request update',
         html: `
-            Hello ${senderusername}, your request to ${travelerusername} on the pkloop platform was just accepted. You can reach the traveler via email or phone to decide on meeting point and other necessary details.
+        Hello ${senderusername}, your request to ${travelerusername} on the pkloop platform was just accepted. You can reach the traveler via email or phone to decide on meeting point and other necessary details.
 
             Traveler Email: ${traveleremail}
-            Traveler Phone: ${travelerphone}
+      Traveler Phone: ${travelerphone}
 
-            Also, your secret code for this transaction is 458, ensure this is only given to the traveler upon receipt by the receiver of your parcel. 
+      Also, your secret code for this transaction is 458, ensure this is only given to the traveler upon receipt by the receiver of your parcel.
 
-            Please ensure to keep with all the instructions on the pkloop platform to ensure safe delivery of your parcel.
+      Please ensure to keep with all the instructions on the pkloop platform to ensure safe delivery of your parcel.
 
-            The gods be with you.
-        `
+      The gods be with you.`
       }
     case 'travelerAccept':
       emailData = {
         from: process.env.EMAIL_FROM,
         to: senderemail,
-        subject: `Request Update`,
+        subject: 'Request Update',
         html: `
-            Hello ${travelerusername}, Thank you for accepting to help us carry a parcel. Here are some contact details you may be needing for your client.
+         Hello ${travelerusername}, Thank you for accepting to help us carry a parcel.Here are some contact details you may be needing for your client.
 
             Client Email: ${senderemail}
-            Client Phone: ${senderphone}
+          Client Phone: ${senderphone}
 
-            Also, your secret code for this transaction is 065, ensure to obtain the last three digist of this code from the receiver of the parcel upon delivery. Use this to redeem your payment on pkloop. 
+  Also, your secret code for this transaction is 065, ensure to obtain the last three digist of this code from the receiver of the parcel upon delivery.Use this to redeem your payment on pkloop.
 
-            Please ensure to keep with all the instructions on the pkloop platform to ensure safe delivery of your parcel.
-
-            The gods be with you.
-        `
+  Please ensure to keep with all the instructions on the pkloop platform to ensure safe delivery of your parcel.`
       }
     case 'senderReject':
       emailData = {
@@ -71,18 +67,31 @@ const sendMail = async (
   }
 
   try {
-    await sgMail.send(emailData)
+    const response = await sgMail.send(emailData)
 
+    console.log('success', response)
     return 'mail sending success'
-  } catch {
+  } catch (err) {
+    console.log('mail sending failure', err)
     return 'mail sending failure'
   }
+
+  // Hello ${ travelerusername }, Thank you for accepting to help us carry a parcel.Here are some contact details you may be needing for your client.
+
+  //           Client Email: ${ senderemail }
+  // Client Phone: ${ senderphone }
+
+  // Also, your secret code for this transaction is 065, ensure to obtain the last three digist of this code from the receiver of the parcel upon delivery.Use this to redeem your payment on pkloop.
+
+  // Please ensure to keep with all the instructions on the pkloop platform to ensure safe delivery of your parcel.
+
+  // The gods be with you.
 
   // sgMail
   //   .send(emailData)
   //   .then(sent => {
   //     return res.json({
-  //       message: `An email has been sent to ${email}, please follow the instructions to activate`
+  //       message: `An email has been sent to ${senderemail}, please follow the instructions to activate`
   //     })
   //   })
   //   .catch(err => {
