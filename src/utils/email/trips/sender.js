d755571dd580f6c75a2624ef118d09d41e711864
Jephtah <jephtino@gmail.com>
2020-01-30 08:05:50 +0100

    modfied email template

--- src/utils/email/trips/sender.js
+++ src/utils/email/trips/sender.js
@@ -16,13 +16,19 @@ const sendMail = async (
     to: senderemail,
     subject: 'Request update',
     html: `
-        Hello ${senderusername}, your request to ${travelerusername} on the pkloop platform was just accepted. You can reach the traveler via email or phone to decide on meeting point and other necessary details.
-
+        Hello ${senderusername}, 
+        <br />
+        <br />
+        This is to notify you that your request to ${travelerusername} on the pkloop platform was just accepted. You can reach the traveler via email or phone to decide on meeting point and other necessary details.
+        <br />
+        <br />
             Traveler Email: ${traveleremail}
-      Traveler Phone: ${travelerphone}
-
+            Traveler Phone: ${travelerphone}
+        <br />
+        <br />
       Also, your secret code for this transaction is [${code}], ensure this is only given to the traveler upon receipt by the receiver of your parcel.
-
+        <br />
+        <br />
       Please ensure to keep with all the instructions on the pkloop platform to ensure safe delivery of your parcel.
 
       The gods be with you.`
